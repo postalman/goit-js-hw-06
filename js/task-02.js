19f8655a018e7f7a15519a2392bf0d34dff99e23
ChillyRide <sebastianmars78@gmail.com>
2023-04-18 05:43:30 +0300

    3done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  return item;
+});
+
+ingredientsList.append(...items);
+
+console.log(ingredientsList);
