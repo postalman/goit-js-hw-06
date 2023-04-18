811b28d322d685c3579921f2891d0fc4e13fd65c
ChillyRide <sebastianmars78@gmail.com>
2023-04-18 23:16:43 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputText = document.querySelector('#name-input');
+const outputText = document.querySelector('#name-output');
+
+inputText.addEventListener('input', onInput);
+
+function onInput(event) {
+    outputText.textContent = event.currentTarget.value;
+}
