8b5c34ea9cb44dd2701d4f5719d2ac3ad7c53130
ChillyRide <sebastianmars78@gmail.com>
2023-04-18 03:38:21 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const categoriesQuantity = document.querySelectorAll("item");
-console.log(categoriesQuantity);
+const categoriesQuantity = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesQuantity.length}`);
+
+categoriesQuantity.forEach(item => {
+    const categoryName = item.querySelector('h2').textContent;
+    const categoryElements = item.querySelectorAll('li');
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryElements.length}`);
+});
+
