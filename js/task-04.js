926ca7de1a1f0b238d7e57a82777f95903f5646f
ChillyRide <sebastianmars78@gmail.com>
2023-04-18 22:32:23 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,23 @@
  let counterValue = 0;
+
+ const decrementBtn = document.querySelector('[data-action="decrement"]');
+ const incrementBtn = document.querySelector('[data-action="increment"]');
+ const valueEl = document.getElementById('value');
+
+ decrementBtn.addEventListener('click', onDecrement);
+ incrementBtn.addEventListener('click', onIncrement);
+
+ function onDecrement() {
+    counterValue -= 1;
+    updateValue();
+ }
+
+ function onIncrement() {
+    counterValue += 1;
+    updateValue();
+ }
+
+function updateValue() {
+    valueEl.textContent = counterValue;
+}
+ 
