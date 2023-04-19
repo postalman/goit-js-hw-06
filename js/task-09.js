6f417cc13adb9fabd3611b018463ff8356998e1c
ChillyRide <sebastianmars78@gmail.com>
2023-04-19 22:10:53 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const spanColor = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', onChange);
+
+function onChange() {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.background = randomColor;
+  spanColor.textContent = randomColor;
+}
