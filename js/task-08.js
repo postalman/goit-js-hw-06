e97a2df369a80964cff81d03a32813d981c41f4d
ChillyRide <sebastianmars78@gmail.com>
2023-04-19 21:56:58 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onSubmit);
+
+const formData = {};
+
+function onSubmit(event) {
+  event.preventDefault();
+  onAlert(event);
+  onFormData(event);
+  loginForm.reset();
+}
+
+function onAlert(event) {
+  if (
+    !event.currentTarget.email.value ||
+    !event.currentTarget.password.value
+  ) {
+    alert(`Всі поля повинні бути заповнені`);
+  }
+}
+
+function onFormData(event) {
+  formData[event.currentTarget.elements.email.name] =
+    event.currentTarget.elements.email.value;
+  formData[event.currentTarget.elements.password.name] =
+    event.currentTarget.elements.password.value;
+}
