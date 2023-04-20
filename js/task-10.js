8a3a27a11e7d80f33f2ee3d30cadd8ea8f801fc8
ChillyRide <sebastianmars78@gmail.com>
2023-04-20 18:02:31 +0300

    10 done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+input.addEventListener("input", onInput);
+
+let amount = 0;
+
+function onInput() {
+  return input.value;
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
+
+function createBoxes(amount) {
+  amount = onInput();
+  boxes.innerHTML = "";
+  let width = 30;
+  let heigth = 30;
+
+   
+  for (let i = 0; i< amount; i+=1) {
+    boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div style="
+    width: ${width}px;
+    height: ${heigth}px;
+    background: ${getRandomHexColor()};
+  "></div>`
+    );
+
+    width += 10;
+    heigth += 10;
+    
+  }
+}
